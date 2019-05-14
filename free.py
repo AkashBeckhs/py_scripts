da616704732f234c388b215bc91992eb47c798b3
Akash <akash.insignia@gmail.com>
2019-05-15 00:04:03 +0530

    written to csv

--- free.py
+++ free.py
@@ -5,7 +5,7 @@ import requests
 import os
 from time import sleep
 import csv
-import datetime
+from datetime import datetime
 
 def RemoveNonAscii(text):
     res=''.join([i if ord(i) < 128 else ' ' for i in text]).strip()
@@ -16,7 +16,13 @@ def RemoveNonAscii(text):
 nameRe="\"nom\":\"(.*?)\","
 priceRe="\"prixPromo\":\"(.*?)\""
 
-proxies={"http":"http://10.135.0.26:8080/","https":"http://10.135.0.26:8080/"}
+
+
+headerName='-----NAME-------'
+headerPrice='--------PRICE---------'  
+
+
+
 url_dict={
     "site_1_2_Url":"http://mobile.free.fr/",
     "site_1_1_Url":"https://www.free.fr/freebox/",
@@ -37,15 +43,11 @@ currDir = os.path.dirname(os.path.realpath(__file__))
 dataList=[]
 
 
-
-
-
-
 def scrapeSite1():
     #part1
     print("1-1 Started")
     s=requests.session()
-    html=s.get(url_dict['site_1_1_Url'],proxies=proxies).text
+    html=s.get(url_dict['site_1_1_Url']).text
     tree=le.fromstring(html)
     names=tree.xpath(xPathDict['name'])
     wd=tree.xpath(xPathDict['w-div'])
@@ -67,14 +69,14 @@ def scrapeSite1():
         l=len(names)
         for i in range(l):
             data=dict()
-            data['Name']=names[i].text
-            data['Price']=prices[i]
+            data[headerName]=names[i].text
+            data[headerPrice]=prices[i]
             dataList.append(data)
     print("1-1 Completed")
     #Part 2 of site 1
     print("1-2 Started")
     s=requests.session()
-    html=s.get(url_dict['site_1_2_Url'],proxies=proxies).text
+    html=s.get(url_dict['site_1_2_Url']).text
     tree=le.fromstring(html)
     dataDivs=tree.xpath("//div[@class='grid-c cell-top forfait']")
     for div in dataDivs:
@@ -85,8 +87,8 @@ def scrapeSite1():
         price=RemoveNonAscii(left[0].text.strip())+"."+RemoveNonAscii(right[0].text)
         #print(name[0].text+" "+name2[0].text+" "+price)
         data=dict()
-        data['Name']=str(name[0].text+" "+name2[0].text).strip()
-        data['Price']=price
+        data[headerName]=str(name[0].text+" "+name2[0].text).strip()
+        data[headerPrice]=price
         dataList.append(data)
     print("1-2 Completed")
 
@@ -94,7 +96,7 @@ def scrapeSite1():
 def scrapeSite2():
     print("2-1 Started")
     s=requests.session()
-    html=s.get(url_dict['site_2_1_Url'],proxies=proxies).text
+    html=s.get(url_dict['site_2_1_Url']).text
     tree=le.fromstring(html)
     divs=tree.xpath("//div[@class='overviewCol__inner']")
     for div in divs:
@@ -104,15 +106,15 @@ def scrapeSite2():
         price=RemoveNonAscii(left[0].text.strip())+"."+RemoveNonAscii(right[0].text)
         #print(name[0].text+" "+price)
         data=dict()
-        data['Name']=name[0].text
-        data['Price']=price
+        data[headerName]=name[0].text
+        data[headerPrice]=price
         dataList.append(data)
     print("2-1 Completed")
     
     #part 2 
     print("2-2 Started")
     s=requests.session()
-    html=s.get(url_dict['site_2_2_Url'],proxies=proxies).text
+    html=s.get(url_dict['site_2_2_Url']).text
     tree=le.fromstring(html)
     divs=tree.xpath("//div[@class='overviewCol__inner']")
     for div in divs:
@@ -122,8 +124,8 @@ def scrapeSite2():
         price=RemoveNonAscii(left[0].text.strip())+"."+RemoveNonAscii(right[0].text)
         #print("2-2-Mob-  "+name[0].text+" "+price)
         data=dict()
-        data['Name']=name[0].text
-        data['Price']=price
+        data[headerName]=name[0].text
+        data[headerPrice]=price
         dataList.append(data)
     print("2-2 Completed")
     
@@ -132,7 +134,7 @@ def scrapeSite3():
     #PART 1
     print("3-1 Started")
     s=requests.session()
-    html=s.get(url_dict['site_3_1_Url'],proxies=proxies).text
+    html=s.get(url_dict['site_3_1_Url']).text
     tree=le.fromstring(html)
     divs=tree.xpath("//div[contains(@class,'column is-half-tablet is-3-desktop')]")
     for div in divs:
@@ -149,20 +151,20 @@ def scrapeSite3():
         if(isinstance(name,str)):
           #print(name+" "+price)
           data=dict()
-          data['Name']=name
-          data['Price']=price
+          data[headerName]=name
+          data[headerPrice]=price
           dataList.append(data)
         else:
            #print(name[0].text+" "+price)
            data=dict()
-           data['Name']=name[0].text
-           data['Price']=price
+           data[headerName]=name[0].text
+           data[headerPrice]=price
            dataList.append(data) 
     print("3-1 Completed")
     #PART 2
     print("3-2 Started")
     s=requests.session()
-    html=s.get(url_dict['site_3_2_Url'],proxies=proxies).text
+    html=s.get(url_dict['site_3_2_Url']).text
     tree=le.fromstring(html)
     divs=tree.xpath("//div[contains(@class,'offers')]/div[contains(@class,'offer')]")
     for div in divs:
@@ -172,8 +174,8 @@ def scrapeSite3():
         right=div.findall(".//th[@class='sup']")
         price=str(left[0].text)+"."+RemoveNonAscii(str(right[0].text_content()))
         data=dict()
-        data['Name']=name
-        data['Price']=price
+        data[headerName]=name
+        data[headerPrice]=price
         dataList.append(data)
     print("3-2 Completed")
     
@@ -182,7 +184,7 @@ def scrapeSite3():
 def scrapeSite4():
     print("4-1 Started")
     s=requests.session()
-    html=s.get(url_dict['site_4_1_Url'],proxies=proxies).text
+    html=s.get(url_dict['site_4_1_Url']).text
     tree=le.fromstring(html)
     divs=tree.xpath("//div[@class='template header-crem']")
     for div in divs:
@@ -192,37 +194,46 @@ def scrapeSite4():
         price=RemoveNonAscii(left[0].text.strip())+"."+RemoveNonAscii(right[0].text).replace(",",'')
        # print(name[0].text+"   "+price)
         data=dict()
-        data['Name']=name[0].text
-        data['Price']=price
+        data[headerName]=name[0].text
+        data[headerPrice]=price
         dataList.append(data)
     print("4-1 Completed")
     #PART 2
     print("4-2 Started")
     s=requests.session()
-    html=s.get(url_dict['site_4_2_Url'],proxies=proxies).text
+    html=s.get(url_dict['site_4_2_Url']).text
     names=re.findall(nameRe,html)
     prices=re.findall(priceRe,html)
     if(len(names)==len(prices)):
         for i in range(len(names)):
             #print(names[i]+"--->"+prices[i])
             data=dict()
-            data['Name']=names[i]
-            data['Price']=prices[i].replace(',','.')
+            data[headerName]=names[i]
+            data[headerPrice]=prices[i].replace(',','.')
             dataList.append(data)
     print("4-2 Completed")
-        
+      
 def init():
     try:
-        di=currDir+"\data"
+        dat=datetime.date(datetime.now())
+        di={headerName:dat,headerPrice:"----"}
+        dataList.append(di)
+        di=currDir+"/data"
         os.mkdir(di)
     except:
         print("Folder already exist")
 
-
 def writeListToCsv(toCSV):
+    oldData=[]
     keys = toCSV[0].keys()
-    dt=datetime.datetime.now()
-    fileName=currDir+"\\data\\"+str(dt).replace('.','_').replace(' ','_').replace(":","_")+".csv"
+    fileName=currDir+"/data/data.csv"
+    xists = os.path.isfile(fileName)
+    if(xists):
+        with open(fileName, "r") as f:
+            reader = csv.DictReader(f)
+            oldData=list(reader)
+    #appending old data with new Data
+    toCSV=toCSV+oldData        
     with open(fileName, 'w') as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
